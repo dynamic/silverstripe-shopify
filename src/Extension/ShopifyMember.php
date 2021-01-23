ff9e46d93e24eba0060ec32372f75bd1e2d379e7
Jason Irish <jsirish@users.noreply.github.com>
2021-01-22 20:05:07 -0600

    FEATURE ShopifyMember (#25)

---
+++ src/Extension/ShopifyMember.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dynamic\Shopify\Extension;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * Class ShopifyMember
+ * @package Dynamic\Shopify\Extension
+ */
+class ShopifyMember extends DataExtension
+{
+    /**
+     * @var string[]
+     */
+    private static $db = [
+        'ShopifyID' => 'Int',
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab(
+            'Root.Shopify',
+            [
+                $fields->dataFieldByName('ShopifyID')
+            ]
+        );
+    }
+}

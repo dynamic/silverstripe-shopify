8265fc21d724df205e6c6bc1047a7cc7c3618937
Jason Irish <jsirish@users.noreply.github.com>
2021-02-01 19:18:16 -0600

    FEATURE BuyButton integration initial (#31)

---
+++ src/Extension/ShopifyExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Dynamic\Shopify\Extension;
+
+use Dynamic\Shopify\Client\ShopifyClient;
+use Dynamic\Shopify\Page\ShopifyProduct;
+use SilverStripe\Core\Convert;
+use SilverStripe\Core\Extension;
+use SilverStripe\ORM\FieldType\DBCurrency;
+use SilverStripe\View\Requirements;
+
+/**
+ * Class ShopifyExtension
+ * @package Dynamic\Shopify\Extension
+ */
+class ShopifyExtension extends Extension
+{
+    /**
+     * @return mixed
+     */
+    public function getCartOptions()
+    {
+        return Convert::array2json(array_merge_recursive(ShopifyProduct::config()->get('options'), [
+            'cart' => [
+                'text' => [
+                    'title' => _t('Shopify.CartTitle', 'Cart'),
+                    'empty'=> _t('Shopify.CartEmpty', 'Your cart is empty.'),
+                    'button' => _t('Shopify.CartButton', 'Checkout'),
+                    'total' => _t('Shopify.CartTotal', 'Subtotal'),
+                    'currency' => ShopifyProduct::config()->get('currency'),
+                    'notice' => _t('Shopify.CartNotice', 'Shipping and discount codes are added at checkout.')
+                ]
+            ]
+        ]));
+    }
+
+    public function onAfterInit()
+    {
+        if (ShopifyClient::config()->get('inject_javascript') !== false) {
+            $domain = ShopifyClient::config()->get('shopify_domain');
+            $accessToken = ShopifyClient::config()->get('storefront_access_token');
+            $currencySymbol = DBCurrency::config()->get('currency_symbol');
+            Requirements::javascript('//sdks.shopifycdn.com/buy-button/latest/buybutton.js');
+            Requirements::customScript(<<<JS
+            (function () {
+                var client = ShopifyBuy.buildClient({
+                  domain: '{$domain}',
+                  storefrontAccessToken: '{$accessToken}'
+                });
+
+                window.shopifyClient = ShopifyBuy.UI.init(client);
+                window.shopifyClient.createComponent('cart', {
+                   node: document.getElementById('shopify-cart'),
+                   moneyFormat: '$currencySymbol{{amount}}',
+                   options: {$this->getCartOptions()}
+                });
+            })();
+JS
+            );
+        }
+    }
+}

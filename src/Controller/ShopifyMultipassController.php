9c911982e60c8694ee726bbd5603be0b8202bb83
Jason Irish <jirish@dynamicagency.com>
2021-04-12 11:58:08 -0500

    check email is valid prior to redirect

--- src/Controller/ShopifyMultipassController.php
+++ src/Controller/ShopifyMultipassController.php
@@ -30,23 +30,25 @@ class ShopifyMultipassController extends Controller
 
         if ($multipass_secret = ShopifyMultipass::config()->get('multipass_secret')) {
             if ($member = Security::getCurrentUser()) {
-                $domain = ShopifyClient::config()->get('shopify_domain');
-                $token_date = new \DateTime('NOW');
-                $token_date = $token_date->format('c'); // ISO8601 formated datetime
-                $request = $this->getRequest();
-                $customer_data = array(
-                    "email" => $member->Email,
-                    "created_at" => $token_date,
-                    "first_name" => $member->FirstName,
-                    "last_name" => $member->Surname,
-                    "return_to" => $return_url,
-                    //"remote_ip" => $request->getIP(),
-                );
-                
-                $multipass = new ShopifyMultipass($multipass_secret);
-                $token = $multipass->generate_token($customer_data);
+                if (filter_var($member->Email, FILTER_VALIDATE_EMAIL)) {
+                    $domain = ShopifyClient::config()->get('shopify_domain');
+                    $token_date = new \DateTime('NOW');
+                    $token_date = $token_date->format('c'); // ISO8601 formated datetime
+                    $request = $this->getRequest();
+                    $customer_data = array(
+                        "email" => $member->Email,
+                        "created_at" => $token_date,
+                        "first_name" => $member->FirstName,
+                        "last_name" => $member->Surname,
+                        "return_to" => $return_url,
+                        //"remote_ip" => $request->getIP(),
+                    );
 
-                return $this->redirect("https://{$domain}/account/login/multipass/{$token}");
+                    $multipass = new ShopifyMultipass($multipass_secret);
+                    $token = $multipass->generate_token($customer_data);
+
+                    return $this->redirect("https://{$domain}/account/login/multipass/{$token}");
+                }
             }
         }
         return $this->redirect($return_url);

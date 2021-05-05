8d0a8a7f29e9f4053d04282c0ca99b7e6e7055c2
mak001 <mak001@users.noreply.github.com>
2021-05-05 12:03:19 -0500

    Added back url support for multipass (#89)
    - allows logging in and redirecting back to checkout

--- src/Extension/ShopifyLoginHandler.php
+++ src/Extension/ShopifyLoginHandler.php
@@ -2,6 +2,7 @@
 
 namespace Dynamic\Shopify\Extension;
 
+use Dynamic\Shopify\Client\ShopifyClient;
 use Dynamic\Shopify\Client\ShopifyMultipass;
 use SilverStripe\Control\Director;
 use SilverStripe\Security\MemberAuthenticator\LoginHandler;
@@ -20,8 +21,32 @@ class ShopifyLoginHandler extends LoginHandler
         $multipass_secret = ShopifyMultipass::config()->get('multipass_secret');
         if (isset($multipass_secret)) {
             $url = Director::absoluteBaseURL() . 'multipass';
+            print_r($this->getRequest());
+            if ($backURL = $this->getBackURL()){
+                $url = $url . '?BackURL=' . $backURL;
+            } else if ($backURL = $this->getOffSiteBackURL('postVar')) {
+                $url = $url . '?BackURL=' . $backURL;
+            } else if ($backURL = $this->getOffSiteBackURL('getVar')) {
+                $url = $url . '?BackURL=' . $backURL;
+            }
             return $this->redirect($url);
         }
         return parent::redirectAfterSuccessfulLogin();
+    }
+
+    /**
+     * So we can redirect back to checkout or cart. Will still only allow redirects to current site or shopify
+     * @param string $method
+     * @return bool|string
+     */
+    private function getOffSiteBackURL($method)
+    {
+        if ($backURL = $this->getRequest()->{$method}('BackURL')) {
+            $host = parse_url($backURL, PHP_URL_HOST);
+            if ($host === ShopifyClient::get_domain()) {
+                return $backURL;
+            }
+        }
+        return false;
     }
 }

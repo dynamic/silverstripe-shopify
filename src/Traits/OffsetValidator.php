0445dcfb18a05ec61b40c3a6c5113e937d9e447e
mak001 <mak001001@gmail.com>
2021-08-03 13:11:48 -0500

    started to add better checks on data   - helps prevent errors when mapping

---
+++ src/Traits/OffsetValidator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dynamic\Shopify\Traits;
+
+use Osiset\BasicShopifyAPI\ResponseAccess;
+
+trait OffsetValidator
+{
+    /**
+     * @param array $data
+     *
+     * @return bool
+     */
+    public static function hasData($response)
+    {
+        return $response && array_key_exists('body', $response) && $response['body']->offsetExists('data');
+    }
+
+    /**
+     * @param array $response
+     *
+     * @return array|ResponseAccess|bool
+     */
+    public function getData($response)
+    {
+        if (!self::hasData($response)) {
+            return false;
+        }
+        return $response['body']->data;
+    }
+
+    /**
+     * @param array|ResponseAccess $data
+     * @param array|string $offsets
+     *
+     * @return bool
+     */
+    public static function offsetExists($data, $offsets)
+    {
+        if (!is_array($offsets)) {
+            $offsets = [$offsets];
+        }
+
+        $currentOffset = array_shift($offsets);
+        if (!$data->offsetExists($currentOffset)) {
+            return false;
+        }
+
+        if (count($offsets)) {
+            return self::offsetExists($data->{$currentOffset}, $offsets);
+        }
+    }
+}
